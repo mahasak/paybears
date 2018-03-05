de2473625d605e6391c169abe6bc8c2f839525de
Mahasak Pijittum <mahasak@gmail.com>
2018-03-06 00:14:53 +0700

    add Hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import './Burger.css';
+import { slide as Menu } from 'react-burger-menu'
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Menu  >
+        <a id="home" className="menu-item" href="/">Home</a>
+        <a id="about" className="menu-item" href="/about">About</a>
+        <a id="contact" className="menu-item" href="/contact">Contact</a>
+        </Menu>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
