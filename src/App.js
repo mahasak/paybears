c8f110657e929c43648aa53f3a3a74e774a48821
Mahasak Pijittum <mahasak@gmail.com>
2018-03-06 00:20:14 +0700

    Add page-wrap id

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './Burger.css';
-import { slide as Menu } from 'react-burger-menu'
+import { push as Menu } from 'react-burger-menu'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Menu  >
+        <Menu  pageWrapId={ "page-wrap" }>
         <a id="home" className="menu-item" href="/">Home</a>
         <a id="about" className="menu-item" href="/about">About</a>
         <a id="contact" className="menu-item" href="/contact">Contact</a>
         </Menu>
+        <main id="page-wrap">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -19,6 +20,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        </main>
       </div>
     );
   }
